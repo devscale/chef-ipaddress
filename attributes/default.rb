33da8b74c899488bcc545c4d73aad11a8c52040a
mohitsethi <mohit@sethis.in>
2013-05-09 17:13:27 +0530

    updated attributes:default, recipes

---
+++ attributes/default.rb
@@ -0,0 +1,1 @@
+default['interface']['id'] = 'eth0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,21 +4,25 @@ if nodename == nil
 	nodename = node[:hostname]
 end
 
-db = data_bag_item("servers",nodename)
-interfaces = db['interfaces']
+case node['platform_family']
+when 'debian', 'ubuntu'
+
+	db = data_bag_item("servers",nodename)
+	interfaces = db['interfaces']
 
 
-service "networking" do
-	service_name "networking"
-  action :restart
-end
+	service "networking" do
+		service_name "networking"
+	  action :restart
+	end
 
-template "/etc/network/interfaces" do
-	source "interfaces.erb"
-	owner "root"	
-	group "root"
-  mode "0644"
-  action :create
-	variables :interfaces => interfaces
-	#notifies :start, "service[networking]", :immediately
+	template "/etc/network/interfaces" do
+		source "interfaces.erb"
+		owner "root"	
+		group "root"
+	  mode "0644"
+	  action :create
+		variables :interfaces => interfaces
+		#notifies :start, "service[networking]", :immediately
+	end
 end
